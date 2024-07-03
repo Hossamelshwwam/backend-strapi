1518035e5aa311e51feda4891c73eb2c9427649b
hossam <you@example.com>
2024-07-03 12:41:45 +0300

    first commit

---
+++ src/api/site-data/controllers/site-data.ts
@@ -0,0 +1,7 @@
+/**
+ * site-data controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::site-data.site-data');
---
+++ src/api/site-data/services/site-data.ts
@@ -0,0 +1,7 @@
+/**
+ * site-data service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::site-data.site-data');
